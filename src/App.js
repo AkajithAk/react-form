1d8e30df1696e7a898fc93ee66c2c419ba56aca8
AkajithAk <akajith919@gmail.com>
2021-08-31 16:58:38 +0530

    added button and apply some styles in the form

--- src/App.js
+++ src/App.js
@@ -12,19 +12,23 @@ import Demo from './demo';
 function App() { 
    const [names,setName]=useState();
    const [email,setEmail]=useState();
+   const [select,setSelect]=useState();
+   const [gender,setGender]=useState();
    const [combine,setCombine]=useState([]); 
-  
-  
+   
+
+   console.log(gender);
+   
 
   
 const DataSubmited=(e)=>{
   e.preventDefault();
-  const value={names,email};
-  if(names&&email){
+  const value={names,email,select,gender};
+  if(names&&email&&select&&gender){
     setCombine(datas=>[...datas,value])
     setName("")
     setEmail("")
-  }
+  } 
 }
 
 
@@ -32,9 +36,33 @@ const DataSubmited=(e)=>{
   return (
     <div className="App">
       <form onSubmit={DataSubmited}>
-        <input type="text"   value={names} onChange={e=>setName(e.target.value)}/>
-        <input type="text"  value={email} onChange={e=>setEmail(e.target.value)}/>
-        <button>submit</button>
+        <div className="input-pading">
+        <input className="inputs" type="text"  placeholder="Name" value={names} onChange={e=>setName(e.target.value)}/>
+       
+        </div>
+        <div className="input-pading">
+        <input className="inputs" type="text"  placeholder="Email" value={email} onChange={e=>setEmail(e.target.value)}/>
+       
+        </div>
+      <div className="input-pading">
+      Rating : 
+        <select value={select} onChange={e=>setSelect(e.target.value)}>
+      <option value="Luke Skywalker">
+       1
+      </option>
+      <option value="C-3PO">2</option>
+      <option value="R2-D2">3</option>
+      <option value="R2-D2">4</option>
+      <option value="R2-D2">5</option>
+       
+    </select>
+      </div>
+      <br/>
+      Gender : 
+    <input   type="radio" value="male"   name="gender" onChange={e=>setGender(e.target.value)}/>Male
+    <input  type="radio" value="female"  name="gender" onChange={e=>setGender(e.target.value)}/>Female
+       <br/><br/>
+        <button className="submit">submit</button>
         </form>
        
         <Router>
--- src/list.js
+++ src/list.js
@@ -1,10 +1,20 @@
 import React from "react";
+import './App.css';
 function List(name){  
+    console.log(name); 
 return(
     <>
-    {name.combine.map(e=><div>
-        <h1>{e.names}</h1>
-        <h1>{e.email}</h1>
+    {name.combine.map(e=><div className="main-div">
+       <div className="cards">
+        <h1>Name: {e.names}</h1>
+        
+      <h1> Email : {e.email}</h1>
+      <h1> Ratings :{e.select}</h1>
+       <h1>Gender : {e.gender}</h1>
+       <button className="edit">Edit</button>&nbsp;&nbsp;
+       <button className="delete">Delete</button></div>
+       
+
     </div>)} 
     
     </>
